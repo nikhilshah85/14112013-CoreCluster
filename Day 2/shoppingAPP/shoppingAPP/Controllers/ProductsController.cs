4465342d35726fd5addd1d995b4621338fa5f371
Nikhil Shah <nikhilshah.net@hotmail.com>
2023-11-15 13:23:55 +0530

    Day 2

---
+++ Day 2/shoppingAPP/shoppingAPP/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoppingAPP.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        public IActionResult ProductList()
+        {
+            //collect data from Models
+
+
+            List<string> productsCategory = new List<string>()
+            {
+                "TShirts","Shoes","Accessories","Ëlectronics","Jeans","Mobiles"
+            };
+
+            List<string> products = new List<string>()
+            {
+                "Nike","Apple","Adidas","Samsung Galaxy","Flying Machine","Super Dry"
+            };
+
+            ViewBag.ProductsCategory = productsCategory;
+            ViewBag.Products = products;
+
+
+
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult HotSellingProducts()
+        {
+            return View();
+        }
+
+        public IActionResult Offers()
+        {
+            return View();
+        }
+
+
+
+
+      
+    }
+}
