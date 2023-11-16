167f4e1a8e84c4d08318fef552598b14ab9a65cc
Nikhil Shah <nikhilshah.net@hotmail.com>
2023-11-16 13:25:13 +0530

    Day 3

---
+++ Day 3/shoppingAPP_day3/shoppingAPP_day3/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPP_day3.Models;
+namespace shoppingAPP_day3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        ProductsModel _productsModel = new ProductsModel(); //this is a crime, we should use DI instead, which we will study later
+
+
+        public IActionResult ViewAllProducts()
+        {
+            ViewBag.Products = _productsModel.GetAllProducts();
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ViewProductById() 
+        {
+            ViewBag.ProductById = null;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ViewProductById(int id) 
+        {
+
+            try
+            {
+                ViewBag.ProductById = _productsModel.GetProductById(id);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
+        }
+
+
+
+
+
+
+        public IActionResult ViewProductByCategory(string category)
+        {
+            try
+            {
+                ViewBag.Category = category;
+                return View();
+            } catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
