fcc844346cbf3933f720e7ce931208f6981a6877
Nikhil Shah <nikhilshah.net@hotmail.com>
2023-11-17 13:27:22 +0530

    Day 4

---
+++ Day 4/shoppingAPP_day4/shoppingAPP_day3/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPP_day3.Models;
+namespace shoppingAPP_day3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        ProductsModel _productsModel = new ProductsModel(); //this is a crime, we should use DI instead, which we will study later
+
+
+        public IActionResult ViewAllProducts()
+        {
+            ViewBag.Products = _productsModel.GetAllProducts();
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ViewProductById() 
+        {
+            ViewBag.ProductById = null;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ViewProductById(int id) 
+        {
+
+            try
+            {
+                ViewBag.ProductById = _productsModel.GetProductById(id);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
+        }
+
+
+
+
+        public IActionResult ViewProductByCategory()
+        {
+            ViewBag.Category = null;
+            return View();
+        }
+
+
+       [HttpPost]
+        public IActionResult ViewProductByCategory(string category)
+        {
+            try
+            {
+                ViewBag.Category = _productsModel.GetProductsByCategory(category);
+                return View();
+            } catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Day 4/shoppingAPP_day4/shoppingAPP_day3/Models/ProductsModel.cs
@@ -0,0 +1,128 @@
+﻿using System.Reflection;
+
+namespace shoppingAPP_day3.Models
+{
+    public class ProductsModel
+    {
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+        #endregion
+
+        #region Collection Data - Model
+
+        private static List<ProductsModel> _productsList = new List<ProductsModel>()
+        {
+            new ProductsModel { pId = 1, pName = "Iphone", pCategory = "Smart Phone", pIsInStock = true, pPrice = 150000 },
+            new ProductsModel { pId = 2, pName = "AirPods", pCategory = "Accessory", pIsInStock = false, pPrice = 26000 },
+            new ProductsModel { pId = 3, pName = "Nike Air", pCategory = "Shoes", pIsInStock = true, pPrice = 12000 },
+            new ProductsModel { pId = 4, pName = "Pepsi", pCategory = "Cold-Drink", pIsInStock = true, pPrice = 80 },
+            new ProductsModel { pId = 5, pName = "Maggie", pCategory = "FastFood", pIsInStock = true, pPrice = 120 },
+            new ProductsModel { pId = 6, pName = "Super Dry", pCategory = "TShirt", pIsInStock = false, pPrice = 2800 },
+            new ProductsModel { pId = 7, pName = "Jeep Compass", pCategory = "SUV", pIsInStock = true, pPrice = 2800000 },
+            new ProductsModel { pId = 7, pName = "Jeep Compass", pCategory = "SUV", pIsInStock = true, pPrice = 2800000 },
+            new ProductsModel { pId = 7, pName = "Puma Sports TShirt", pCategory = "TShirt", pIsInStock = true, pPrice = 2800000 },
+            new ProductsModel { pId = 7, pName = "GAP", pCategory = "TShirt", pIsInStock = true, pPrice = 2800000 },
+        };
+
+        #endregion
+
+        #region Select / Get data
+        public List<ProductsModel> GetAllProducts()
+        {
+            return _productsList;
+        }
+
+        public ProductsModel GetProductById(int id)
+        {
+            var p = _productsList.Find(p => p.pId == id);
+            if (p == null)
+            {
+                throw new Exception("Product not found");
+            }
+            return p;
+        }
+
+        //public List<string> GetUniqueProductCategory()
+        //{
+        //   var categories = (from p in _productsList
+        //                    select p).Distinct(c => c.pCategory);
+                
+        //}
+
+        public List<ProductsModel> GetProductsByCategory(string category)
+        {
+            var p = _productsList.FindAll(p => p.pCategory == category);
+            if (p == null)
+            {
+                throw new Exception("No product found");
+            }
+            return p;
+        }
+
+        #endregion
+
+        #region Add, Update and Delete methods
+        public string AddNewProduct(ProductsModel model)
+        {
+            //we can do data validation here before adding the model to collection/db 
+            //eg. if(model.pName.Length < 3){ thrown new exception("message")}
+            _productsList.Add(model);
+            return "Product Added Successfully";
+        }
+
+        public string EditProduct(ProductsModel model)
+        {
+            var p = _productsList.Find(pr => pr.pId == model.pId);
+            if (p != null)
+            {
+                p.pName = model.pName;
+                p.pCategory = model.pCategory;
+                p.pPrice = model.pPrice;
+                p.pIsInStock = model.pIsInStock;
+                return "Product Details moadified successfully";
+
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public string DeleteProduct(int id)
+        {
+            var p = _productsList.Find(pr => pr.pId == id);
+            if (p == null)
+            {
+                throw new Exception("Product Not Found");
+            }
+            _productsList.Remove(p);
+            return "Product Deleted Successfully";
+        }
+        #endregion
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
